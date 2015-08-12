4e93ab9a1c0cd1f3b8eedbbddade9845387d2446
Matias Perrone <matias.perrone@globalinnovation.com.ar>
2015-08-12 16:57:39 -0300

    v1.4.1

--- iCalEasyReader.php
+++ iCalEasyReader.php
@@ -7,7 +7,7 @@
  * @author		Matias Perrone <matias.perrone at gmail dot com>
  * @author		Timo Henke <phpstuff@thenke.de> (Some ideas taken partially from iCalParse on http://www.phpclasses.org/)
  * @license		http://www.opensource.org/licenses/mit-license.php MIT License
- * @version		1.4
+ * @version		1.4.1
  * @param	string	$data	ics file string content
  * @param	array|false	$data $makeItEasy	the idea is to convert this "keys" into the "values", converting the DATE and DATE-TIME values to the respective DateTime type of PHP, also all the keys are lowercased
  * @return	array|false
@@ -15,15 +15,16 @@
 class iCalEasyReader
 {
 	private $ical = null;
+	private $_lastitem = null;
 
 	public function &load($data)
 	{
 		$this->ical = false;
 		$regex_opt = 'mib';
-		
+
 		// Lines in the string
 		$lines = mb_split( '[\r\n]+', $data );
-		
+
 		// Delete empty ones
 		$last = count( $lines );
 		for($i = 0; $i < $last; $i ++)
@@ -32,11 +33,11 @@ class iCalEasyReader
 				unset( $lines[$i] );
 		}
 		$lines = array_values( $lines );
-		
+
 		// First and last items
 		$first = 0;
 		$last = count( $lines ) - 1;
-		
+
 		if (! ( mb_ereg_match( '^BEGIN:VCALENDAR', $lines[$first], $regex_opt ) and mb_ereg_match( '^END:VCALENDAR', $lines[$last], $regex_opt ) ))
 		{
 			$first = null;
@@ -45,7 +46,7 @@ class iCalEasyReader
 			{
 				if (mb_ereg_match( '^BEGIN:VCALENDAR', $line, $regex_opt ))
 					$first = $i;
-				
+
 				if (mb_ereg_match( '^END:VCALENDAR', $line, $regex_opt ))
 				{
 					$last = $i;
@@ -53,12 +54,12 @@ class iCalEasyReader
 				}
 			}
 		}
-		
+
 		// Procesing
 		if (! is_null( $first ) and ! is_null( $last ))
 		{
 			$lines = array_slice( $lines, $first + 1, ( $last - $first - 1 ), true );
-			
+
 			$group = null;
 			$parentgroup = null;
 			$this->ical = [];
@@ -72,8 +73,6 @@ class iCalEasyReader
 				$regs = mb_ereg_search_regs( $pattern, $regex_opt );
 				if ($regs)
 				{
-					var_dump( $regs );
-					die();
 					// $regs
 					// 0 => BEGIN:VEVENT
 					// 1 => BEGIN
@@ -83,9 +82,9 @@ class iCalEasyReader
 						case 'BEGIN' :
 							if (! is_null( $group ))
 								$parentgroup = $group;
-							
+
 							$group = trim( $regs[2] );
-							
+
 							// Adding new values to groups
 							if (is_null( $parentgroup ))
 							{
@@ -98,43 +97,43 @@ class iCalEasyReader
 							{
 								if (! array_key_exists( $parentgroup, $this->ical ))
 									$this->ical[$parentgroup] = [$group => [null]];
-								
+
 								if (! array_key_exists( $group, $this->ical[$parentgroup] ))
 									$this->ical[$parentgroup][$group] = [null];
 								else
 									$this->ical[$parentgroup][$group][] = null;
 							}
-							
+
 							break;
 						case 'END' :
 							if (is_null( $group ))
 								$parentgroup = null;
-							
+
 							$group = null;
 							break;
 					}
 					continue;
 				}
-				
+
 				if (! in_array( $line[0], [" ", "\t"] ))
 					$this->addItem( $line, $group, $parentgroup );
 				else
 					$this->concatItem( $line );
 			}
 		}
-		
+
 		return $this->ical;
 	}
 
 	public function addType(&$value, $item)
 	{
 		$type = explode( '=', $item );
-		
+
 		if (count( $type ) > 1 and $type[0] == 'VALUE')
 			$value['type'] = $type[1];
 		else
 			$value[$type[0]] = $type[1];
-		
+
 		return $value;
 	}
 
@@ -159,7 +158,7 @@ class iCalEasyReader
 				$value = ['value' => $item[1]];
 				$this->addType( $value, $subitem[1] );
 			}
-			
+
 			// Multi value
 			if (is_string( $value ))
 			{
@@ -175,7 +174,7 @@ class iCalEasyReader
 				}
 				unset( $z );
 			}
-			
+
 			if (is_null( $parentgroup ))
 			{
 				$this->ical[$group][count( $this->ical[$group] ) - 1][$subitem[0]] = $value;
@@ -208,7 +207,7 @@ class iCalEasyReader
 	{
 		$patterns = ['\\\\[n]', '\\\\[t]', '\\\\,', '\\\\;'];
 		$replacements = ["\n", "\t", ",", ";"];
-		
+
 		return $this->mb_eregi_replace_all( $patterns, $replacements, $line );
 	}
 
@@ -222,13 +221,13 @@ class iCalEasyReader
 					$reemplazo = $replacement[$i];
 				else
 					$reemplazo = '';
-				
+
 				$string = mb_eregi_replace( $patron, $reemplazo, $string );
 			}
 		}
 		elseif (is_string( $pattern ) and is_string( $replacement ))
 			$string = mb_eregi_replace( $pattern, $replacement, $string );
-		
+
 		return $string;
 	}
 }
